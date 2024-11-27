c2a8e617c8b83a5a728996de154ead509d03fdc9
Sulochan Mahajan <127333103+Sulochan36@users.noreply.github.com>
2024-11-27 08:02:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Doubly linked list Insertion at given position/doubly-linked-list-insertion-at-given-position.cpp
+++ Difficulty: Basic/Doubly linked list Insertion at given position/doubly-linked-list-insertion-at-given-position.cpp
@@ -3,87 +3,20 @@
 using namespace std;
 
 /* a Node of the doubly linked list */
-struct Node
-{
-  int data;
-  struct Node *next;
-  struct Node *prev;
-  Node(int x)
-  {
-      data = x;
-      next = prev = NULL;
-  }
+struct Node {
+    int data;
+    struct Node *next;
+    struct Node *prev;
+
+    Node(int x) {
+        data = x;
+        next = prev = NULL;
+    }
 };
 
-void addNode(Node *head, int pos, int data);
 
-Node *insert(Node *head, int x)
-{
-    if (head == NULL)
-    {
-        return new Node(x);
-    }
-    Node *n = new Node(x);
-    
-    head->next = n;
-    n->prev = head;
-    head = n;
-    return head;
-}
-
-void printList(Node *head)
-{
-  // The purpose of below two loops is
-  // to test the created DLL
-  Node *temp=head;
-  if (temp != NULL) {
-    int ct1=0,ct2=0;
-  while (temp->next!=NULL)
-    temp=temp->next,ct1++;
-  while (temp->prev!=NULL)
-   temp = temp->prev,ct2++;
-   if(ct1!=ct2){
-       cout<<-1<<"\n";
-       return;
-   }
-  }
-  while (temp != NULL)
-  {
-      printf("%d ",temp->data);
-      temp=temp->next;
-  }
-  
-  cout << endl;
-}
-
-int main()
-{
-  int t;
-  scanf("%d",&t);
-  while(t--)
-  {
-  Node *head = NULL; 
-  Node *root = NULL;
-  int n;
-  scanf("%d",&n);
-  for(int i=0;i<n;i++){
-     int x;
-     scanf("%d",&x);
-     head = insert(head, x);
-     if(root==NULL) root = head;
-  }     
-  head = root;
-  int pos,data;
-  cin>>pos>>data;
-  addNode(head, pos, data);
-  printList(head);
-  }
-  return 0;
-}
 // } Driver Code Ends
-
-
-/* a Node of the doubly linked list 
+/* a Node of the doubly linked list
 struct Node
 {
   int data;
@@ -92,28 +25,110 @@ struct Node
   Node(int x) { data = x; next = prev = NULL; }
 }; */
 
-//Function to insert a new node at given position in doubly linked list.
-void addNode(Node* head, int p, int x) {
-    // Traverse to the p-th node
-    Node* current = head;
-    for (int i = 0; i < p && current != nullptr; ++i) {
-        current = current->next;
+// Function to insert a new node at given position in doubly linked list.
+class Solution {
+  public:
+    // Function to insert a new node at given position in doubly linked list.
+    Node *addNode(Node *head, int pos, int data) {
+        // code here
+        Node* temp = head;
+        for(int i=0;i<pos && temp!=NULL;i++){
+            temp=temp->next;
+        }
+        
+        if(temp==NULL){
+            return head;
+        }
+        
+        Node* newNode = new Node(data);
+        
+        newNode->prev = temp;
+        newNode->next = temp->next;
+        
+        if(temp->next!=NULL){
+           newNode->next->prev = newNode; 
+        }
+        
+        temp->next = newNode;
+        
+        return head;
+        
     }
+};
 
-    if (current == nullptr) {
-        // Position p is out of bounds
-        return;
+//{ Driver Code Starts.
+
+Node *insert(Node *head, int x) {
+    if (head == NULL) {
+        return new Node(x);
     }
+    Node *n = new Node(x);
 
-    // Create a new node with value x
-    Node* newNode = new Node(x);
-
-    // Insert the new node after the p-th node
-    newNode->next = current->next;
-    newNode->prev = current;
-
-    if (current->next != nullptr) {
-        current->next->prev = newNode;
-    }
-    current->next = newNode;
+    head->next = n;
+    n->prev = head;
+    head = n;
+    return head;
 }
+
+void printList(Node *head) {
+    // The purpose of below two loops is
+    // to test the created DLL
+    Node *temp = head;
+    if (temp != NULL) {
+        int ct1 = 0, ct2 = 0;
+        while (temp->next != NULL)
+            temp = temp->next, ct1++;
+        while (temp->prev != NULL)
+            temp = temp->prev, ct2++;
+        if (ct1 != ct2) {
+            cout << -1 << "\n";
+            return;
+        }
+    }
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    cout << endl;
+}
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore newline after reading T to use getline properly
+
+    while (t--) {
+        Node *head = NULL;
+        Node *root = NULL;
+        vector<int> arr;
+        string input;
+        getline(cin, input);    // Read the entire line as input
+        stringstream ss(input); // Use stringstream to split the input into integers
+        int number;
+        while (ss >> number) {
+            arr.push_back(number); // Add each number to the vector
+        }
+        if (arr.empty()) {
+            return NULL; // Return NULL if the vector is empty
+        }
+        int n = arr.size();
+        for (int i = 0; i < n; i++) {
+            int x = arr[i];
+            head = insert(head, x);
+            if (root == NULL)
+                root = head;
+        }
+        head = root;
+        int pos, data;
+        cin >> pos >> data;
+        cin.ignore();
+        Solution ob;
+        head = ob.addNode(head, pos, data);
+        printList(head);
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
